dc92fae744ee135e29dd250e80ba5c9ecd5c9c2c
Helena Veles <helena.veles@mail.ru>
2023-09-24 17:21:51 +0300

    Сделана восьмая задача

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int evenNum = 2;
+
+if(num > 1)
+{
+    while(evenNum <= num)
+    {
+        Console.Write(evenNum + " ");
+        evenNum = evenNum + 2;
+    }
+}
