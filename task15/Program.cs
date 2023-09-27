78c07dea5be9ae2ef51e159ea2f389a18ee8b137
Helena Veles <helena.veles@mail.ru>
2023-09-27 22:38:12 +0300

    Сделана пятнадцатая задача

---
+++ task15/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+{
+    if (dayNum == 6 || dayNum == 7) 
+    {
+        Console.WriteLine("Этот день выходной.");
+    }
+    else if (dayNum < 1 || dayNum > 7) 
+    {
+        Console.WriteLine("Это не день недели.");
+    }
+    else Console.WriteLine("Этот день не выходной.");
+}
+
