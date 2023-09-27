caa1e982be618abfe6e1ee3152a8ad032977fb00
Helena Veles <helena.veles@mail.ru>
2023-09-27 21:27:52 +0300

    Сделана десятая задача

---
+++ task10/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string strNumb = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра этого числа: "+strNumb[1]);
