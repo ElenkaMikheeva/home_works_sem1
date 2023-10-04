a4ddef1e0f34b345cf3ca87b09c5070bd2332a4c
Helena Veles <helena.veles@mail.ru>
2023-10-04 22:25:21 +0300

    Сделана двадцать седьмая задача

---
+++ task29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array.Length);
+}
+
+int size = 8;
+int min = 0;
+int max = 9;
+int[] myArray = CreateRandomArray(size, min, max);
+PrintArray(myArray);
+Console.Write(" -> [");
+PrintArray(myArray);
+Console.Write("]");
