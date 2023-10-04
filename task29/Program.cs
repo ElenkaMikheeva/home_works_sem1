dd693d86a23d4d397282172d9e0f66c70cc6e7ea
Helena Veles <helena.veles@mail.ru>
2023-10-04 22:48:39 +0300

    Сделана двадцать девятая задача

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,28 +1,6 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-int[] CreateRandomArray(int size, int minValue, int maxValue)
-{
-    int[] array = new int[size];
-    for(int i = 0; i < size; i++)
-    {
-        array[i] = new Random().Next(minValue, maxValue);
-    }
-    return array;
+int[] array = new int[8];
+for(int i = 0; i < 8; i++){
+    array[i] = 12-i;
 }
-
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        Console.Write(array[i] + ", ");
-    }
-    Console.Write(array.Length);
-}
-
-int size = 8;
-int min = 0;
-int max = 9;
-int[] myArray = CreateRandomArray(size, min, max);
-PrintArray(myArray);
-Console.Write(" -> [");
-PrintArray(myArray);
-Console.Write("]");
+Console.WriteLine("[{0}]", string.Join(", ", array));
